6db3db38cf1981d94a5fd6eb5ab788ac69c08676
viviankwok <104907963+viviankwok@users.noreply.github.com>
2022-06-27 16:54:50 +0800

    imported starter codes

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Parent from "./components/Parent";
+import "./styles.css";
+
+function App() {
+  return (
+    <div>
+      <Parent />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Parent.js
@@ -0,0 +1,7 @@
+import React, { useState } from "react";
+
+const Parent = () => {
+  return <div>Parent component here</div>;
+};
+
+export default Parent;
